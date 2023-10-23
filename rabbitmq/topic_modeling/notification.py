4ca78c60ff84a9a0e5329af01d07a85c5e1541c0
Chen Wang <cwang138@illinois.edu>
2023-10-23 15:01:45 -0500

    61 visualization in the email link doesnt work (#70)
    * add url encoding
    
    * update all the notifications
    
    * update the email module
    
    * notification update
    
    * >=3 instead of ==3

--- rabbitmq/topic_modeling/notification.py
+++ rabbitmq/topic_modeling/notification.py
@@ -2,6 +2,21 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
+import urllib.parse
+
+import contextlib
+
+try:
+    from urllib.parse import urlencode
+
+except ImportError:
+    from urllib import urlencode
+
+try:
+    from urllib.request import urlopen
+
+except ImportError:
+    from urllib2 import urlopen
 
 
 def reformat_sessionURL(sessionURL):
@@ -18,6 +33,16 @@ def reformat_sessionURL(sessionURL):
     new_sessionURL = '/'.join(url_parts)
 
     return new_sessionURL
+
+
+def make_tiny(url):
+    try:
+        request_url = 'http://tinyurl.com/api-create.php?' + urlencode({'url': url})
+        with contextlib.closing(urlopen(request_url)) as response:
+            return response.read().decode('utf-8')
+    except Exception as e:
+        # If there is an error (e.g., network issue or TinyURL service is down), return the original URL
+        return url
 
 
 def notification(toaddr, case, filename, links, sessionURL):
@@ -62,132 +87,145 @@ def notification(toaddr, case, filename, links, sessionURL):
                 fpath[2] = 'Python NetworkX'
             elif fpath[2] == 'classification':
                 fpath[2] = 'Text Classification'
+        else:
+            fpath = []
 
-        if case == 0 or case == 'comment-fail':
-            html = """
-            <html> 
-                <head></head>
-                <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
-            <div>
-                        <p>Dear user (session ID: """ + fpath[0] + """),</p>
-                <p>Your Reddit Comment collection has been terminated.</p>
-                <p>We are using the <b>id</b> and <b>permalink</b> from your Reddit Submission dataset
-                to collect comments and replies. It is most likely you have provide an incomplete Reddit Submission dataset missing these two fields.</p>
-                <p>Please try to reproduce the Reddit Submission with <b>id</b> and <b>permalink</b>, or switch to another dataset.</p>
-                <a href=""" + sessionURL + """>Go to your session...</a>
-                        <br>
-                        <p>Best Regards,</p>
-                        <p>Social Media Macroscope - SMILE </p>
-            </div>
-                </body>
-            </html>
-            """
-            subject = 'Your Reddit Comment collection has failed...'
-        elif case == 1 or case == 'comment-terminate':
-            html = """<html> 
-                        <head></head>
-                        <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
-                            <div>
-                                <p>Dear user (session ID: """ + fpath[0] + """),</p>
-                                <p>Your Reddit Comment collection is exceeding 400 Megabyte, and is terminated due to lack of disk space.</p>
-                                <ul>
-                                    <li>You have requested comments and replies for the Reddit Submission (Post):<b>""" + \
-                   fpath[
-                       3] + """</b>. The partial comments we manage to collect and save will be compressed for you in an .zip file named <a href='""" + links + """'>""" + \
-                   fpath[3] + """-comments.zip</a> (click)</li>    
-                                    <li>In order to download this file, you need to first locate the original submission in the <b>Past Results</b> page in SMILE.
-                                       <a href=""" + reformat_sessionURL(sessionURL) + """>Go to your session.</a> 
+        if len(fpath) >= 3:
+            if case == 0 or case == 'comment-fail':
+                html = f"""
+                <html> 
+                    <head></head>
+                    <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
+                <div>
+                            <p>Dear user (session ID: {fpath[0]}),</p>
+                    <p>Your Reddit Comment collection has been terminated.</p>
+                    <p>We are using the <b>id</b> and <b>permalink</b> from your Reddit Submission dataset
+                    to collect comments and replies. It is most likely you have provide an incomplete Reddit Submission dataset missing these two fields.</p>
+                    <p>Please try to reproduce the Reddit Submission with <b>id</b> and <b>permalink</b>, or switch to another dataset.</p>
+                    <a href="{sessionURL}">Go to your session...</a>
+                            <br>
+                            <p>Best Regards,</p>
+                            <p>Social Media Macroscope - SMILE </p>
+                </div>
+                    </body>
+                </html>
+                """
+                subject = 'Your Reddit Comment collection has failed...'
+            elif case == 1 or case == 'comment-terminate':
+                html = f"""<html> 
+                            <head></head>
+                            <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
+                                <div>
+                                    <p>Dear user (session ID: {fpath[0]}),</p>
+                                    <p>Your Reddit Comment collection is exceeding 400 Megabyte, and is terminated due to lack of disk space.</p>
                                     <ul>
-                                        <li>Go to <b>Past Results</b></li> 
-                                        <li>--> under <b>""" + fpath[1] + """</b></li> 
-                                        <li>--> click <b>""" + fpath[2] + """</b></li> 
-                                        <li>--> then find <b>""" + fpath[3] + """</b></li> 
-                                        <li>--> click <b>VIEW</b></li> 
-                                        <li>--> in the <b>Overview</b> table under the <b>downloadables</b> column, you will find these comments in a zip file.</li>
-                                    </ul>
-                                <ul>
-                                <br>
-                                <p>Best Regards,</p>
-                                <p>Social Media Macroscope - SMILE </p>
-                            </div>
-                        </body>
-                </html>"""
-            subject = 'Your Reddit Comment collection has been terminated...'
-        elif case == 2 or case == 'comment-success':
-            html = """<html> 
-                        <head></head>
-                        <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
-                            <div>
-                                <p>Dear user (session ID: """ + fpath[0] + """),</p>
-                                <p>Your Reddit Comment collection is ready for you!</p>
-                                <ul>
-                                    <li>You have requested comments and replies for the Reddit Submission (Post):<b>""" + \
-                   fpath[
-                       3] + """</b>. It will be compressed for you in an .zip file named <a href='""" + links + """'>""" + \
-                   fpath[3] + """-comments.zip</a></li>    
-                                    <li>In order to download this file, you need to first locate the original submission in the <b>Past Results</b> page in SMILE.
-                                    <a href=""" + reformat_sessionURL(sessionURL) + """>Go to your session.</a>
+                                        <li>You have requested comments and replies for the Reddit Submission (
+                                        Post):<b>{fpath[3]}</b>. The partial comments we manage to collect 
+                                        and save will be compressed for you in an .zip file named <a href="{make_tiny(links)}">
+                                        {fpath[3]}-comments.zip</a> (click)</li>    
+                                        <li>In order to download this file, you need to first locate the original submission in the <b>Past Results</b> page in SMILE.
+                                           <a href="{reformat_sessionURL(sessionURL)}">Go to your session.</a> 
+                                        <ul>
+                                            <li>Go to <b>Past Results</b></li> 
+                                            <li>--> under <b>{fpath[1]}</b></li> 
+                                            <li>--> click <b>{fpath[2]}</b></li> 
+                                            <li>--> then find <b>{fpath[3]}</b></li> 
+                                            <li>--> click <b>VIEW</b></li> 
+                                            <li>--> in the <b>Overview</b> table under the <b>downloadables</b> column, you will find these comments in a zip file.</li>
+                                        </ul>
                                     <ul>
-                                        <li>Go to <b>Past Results</b></li> 
-                                        <li>--> under <b>""" + fpath[1] + """</b></li> 
-                                        <li>--> click <b>""" + fpath[2] + """</b></li> 
-                                        <li>--> then find <b>""" + fpath[3] + """</b></li>
-                                        <li>--> click <b>VIEW</b></li> 
-                                        <li>--> in the <b>Overview</b> table under the <b>downloadables</b> column, you will find these comments in a zip file.</li>
-                                    </ul>
-                                </ul>
-                                <br>
-                                <p>Best Regards,</p>
-                                <p>Social Media Macroscope - SMILE </p>
-                            </div>
-                        </body>
-                </html>"""
-            subject = 'Your Reddit Comment collection is completed!'
-        elif case == 3 or case == 'analytics-success':
-            list_html = ''
-            for key in links.keys():
-                list_html += '<li><a href="' + links[key] + '">' + key + '</a></li>'
-
-            html = """<html> 
-                        <head></head>
-                        <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
-                            <div>
-                                <p>Dear user (session ID: """ + fpath[0] + """),</p>
-                                <p>Your """ + fpath[2] + """ results are ready for you! (job ID: """ + fpath[3] + """)</p>
-                                <ul>
-                                    <li>You can view the visualization and download the results at <b>Past Results</b> page in SMILE. 
-                                    <a href=""" + reformat_sessionURL(sessionURL) + """>Go to your session.</a>
+                                    <br>
+                                    <p>Best Regards,</p>
+                                    <p>Social Media Macroscope - SMILE </p>
+                                </div>
+                            </body>
+                    </html>"""
+                subject = 'Your Reddit Comment collection has been terminated...'
+            elif case == 2 or case == 'comment-success':
+                html = f"""<html> 
+                            <head></head>
+                            <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
+                                <div>
+                                    <p>Dear user (session ID: {fpath[0]}),</p>
+                                    <p>Your Reddit Comment collection is ready for you!</p>
                                     <ul>
-                                        <li>Go to <b>Past Results</b></li>
-                                        <li>--> under <b>""" + fpath[1] + """</b> tab</li>
-                                        <li>--> click <b>""" + fpath[2] + """</b></li>
-                                        <li>--> then find <b>""" + fpath[3] + """</b></li>
-                                        <li>--> click <b>view</b></li>
+                                        <li>You have requested comments and replies for the Reddit Submission (
+                                        Post):<b>{fpath[3]}</b>. It will be compressed for you in an .zip file named 
+                                        <a href="{make_tiny(links)}">{fpath[3]}-comments.zip</a></li>    
+                                        <li>In order to download this file, you need to first locate the original submission in the <b>Past Results</b> page in SMILE.
+                                        <a href="{reformat_sessionURL(sessionURL)}">Go to your session.</a>
+                                        <ul>
+                                            <li>Go to <b>Past Results</b></li> 
+                                            <li>--> under <b>{fpath[1]}</b></li> 
+                                            <li>--> click <b>{fpath[2]}</b></li> 
+                                            <li>--> then find <b>{fpath[3]}</b></li>
+                                            <li>--> click <b>VIEW</b></li> 
+                                            <li>--> in the <b>Overview</b> table under the <b>downloadables</b> column, you will find these comments in a zip file.</li>
+                                        </ul>
                                     </ul>
                                     <br>
-                                    <li>You can also click the link below to download part of the results:
-                                        <ul>""" + list_html + """</ul>
-                                    </li>
-                                </ul>
-                                <br>
-                                <p>Best Regards,</p>
-                                <p>Social Media Macroscope - SMILE </p>
-                            </div>
-                        </body>
-                </html>"""
-            subject = 'Your ' + fpath[2] + ' computation is completed!'
+                                    <p>Best Regards,</p>
+                                    <p>Social Media Macroscope - SMILE </p>
+                                </div>
+                            </body>
+                    </html>"""
+                subject = 'Your Reddit Comment collection is completed!'
+            elif case == 3 or case == 'analytics-success':
+                list_html = ''
+                for key in links.keys():
+                    list_html += f'<li><a href="{make_tiny(links[key])}">{key}</a></li>'
 
-        msg = MIMEMultipart('alternative')
-        msg['Subject'] = subject
-        msg['From'] = fromaddr
-        msg['To'] = toaddr
-        msg.attach(MIMEText(html, 'html'))
+                html = f"""<html> 
+                            <head></head>
+                            <body style="font-size:15px;font-fiamily:'Times New Roman', Times, serif;">
+                                <div>
+                                    <p>Dear user (session ID:{fpath[0]}),</p>
+                                    <p>Your {fpath[2]} results are ready for you! (job ID: {fpath[3]})</p>
+                                    <ul>
+                                        <li>You can view the visualization and download the results at <b>Past Results</b> page in SMILE. 
+                                        <a href="{reformat_sessionURL(sessionURL)}">Go to your session.</a>
+                                        <ul>
+                                            <li>Go to <b>Past Results</b></li>
+                                            <li>--> under <b>{fpath[1]}</b> tab</li>
+                                            <li>--> click <b>{fpath[2]}</b></li>
+                                            <li>--> then find <b>{fpath[3]}</b></li>
+                                            <li>--> click <b>view</b></li>
+                                        </ul>
+                                        <br>
+                                        <li>You can also click the link below to download part of the results:
+                                            <ul>{list_html}</ul>
+                                        </li>
+                                    </ul>
+                                    <br>
+                                    <p>Best Regards,</p>
+                                    <p>Social Media Macroscope - SMILE </p>
+                                </div>
+                            </body>
+                    </html>"""
+                subject = f'Your{fpath[2]}computation is completed!'
+            else:
+                html = None
+                subject = None
+                print("Invalid case! Skip notification.")
 
-        server = smtplib.SMTP(host, int(port))
-        server.starttls()
-        if password is not None and password != "":
-            server.login(fromaddr, password)
-        server.sendmail(fromaddr, toaddr, msg.as_string())
-        server.quit()
+            if html is not None and subject is not None:
+                msg = MIMEMultipart('alternative')
+                msg['Subject'] = subject
+                msg['From'] = fromaddr
+                msg['To'] = toaddr
+                msg.attach(MIMEText(html, 'html'))
+
+                server = smtplib.SMTP(host, int(port))
+                server.starttls()
+                if password is not None and password != "":
+                    server.login(fromaddr, password)
+                server.sendmail(fromaddr, toaddr, msg.as_string())
+                server.quit()
+            else:
+                print("Invalid html content! Skip notification.")
+        else:
+            print("Invalid filepath! Skip notification.")
+
     else:
         print("Invalid Email host setting! Skip notification.")
+
